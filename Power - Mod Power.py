b6831fa8c375f41ab25f667888347d3ba18a844c
Yonathan <47978722+yonathanTesfaye@users.noreply.github.com>
2023-02-06 09:53:16 +0300

    Create Power - Mod Power.py

---
+++ Power - Mod Power.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+print(pow(num1,num2), pow(num1,num2,num3), sep='\n')
