79d38acd5abb380fdf0fe2d22ea13e04c684b74d
Yonathan <47978722+yonathanTesfaye@users.noreply.github.com>
2023-02-06 09:58:17 +0300

    Create Integers Come In All Sizes.py

---
+++ Integers Come In All Sizes.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num1 = int(input())
+pow1 = int(input())
+num2 = int(input())
+pow2 = int(input())
+
+print(pow(num1,pow1) + pow(num2,pow2))
